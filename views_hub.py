0e52f9d37fd720f0b0e00a86ca76d31ea170c6b2
kirishree <115062611+kirishree@users.noreply.github.com>
2025-01-31 10:44:08 +0530

    fixed multiple configuration for same hubip

--- views_hub.py
+++ views_hub.py
@@ -2969,7 +2969,7 @@ def add_cisco_hub(request: HttpRequest):
         if response[0]["message"] == "Successfully Registered" or response[0]["message"] == "This device is already Registered":
             devicename = response[0]["spokedevice_name"]
             devicename = devicename
-            devicehubinfo = coll_hub_info.find_one({"hubdevice_name":devicename})
+            devicehubinfo = coll_hub_info.find_one({"hub_wan_ip_only":data["hub_wan_ip"].split("/")[0]})
             coll_tunnel_ip.delete_many({"uuid":data["uuid"]})
             if not devicehubinfo:
                 devicehubinfo = {}
