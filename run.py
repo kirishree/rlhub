1d024bf6ea4a6fe56701b5ed34e361e53ce950be
kirishree <115062611+kirishree@users.noreply.github.com>
2025-02-05 12:50:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,22 +8,22 @@ db_tunnel = client["reach_link"]
 coll_tunnel_ip = db_tunnel["tunnel_ip"]
 coll_hub_info = db_tunnel["hub_info"]
 data = []
-os.system("systemctl stop reachlink_test")
-for device in coll_hub_info.find({},{"_id":0}):
-        data.append(device)
-with open("/root/reachlink/total_hubs.json", "w") as f:
-        json.dump(data, f)
-        f.close()
-data = []
-for device in coll_tunnel_ip.find({},{"_id":0}):
-        data.append(device)
-with open("/root/reachlink/total_branches.json", "w") as f:
-        json.dump(data, f)
-        f.close()
+#os.system("systemctl stop reachlink_test")
+#for device in coll_hub_info.find({},{"_id":0}):
+#       data.append(device)
+#with open("/root/reachlink/total_hubs.json", "w") as f:
+#        json.dump(data, f)
+#       f.close()
+#data = []
+#for device in coll_tunnel_ip.find({},{"_id":0}):
+#        data.append(device)
+#with open("/root/reachlink/total_branches.json", "w") as f:
+#        json.dump(data, f)
+#        f.close()
 os.system("cp views_hub.py /root/reachlink/reach/views.py")
 os.system("cp com_router_config.py /root/reachlink/")
 os.system("cp urls_hub.py /root/reachlink/reachlink/urls.py")
 os.system("cp reachlinkst.py /root/reachlink/")
-os.system("systemctl restart reachlink_test")
+#os.system("systemctl restart reachlink_test")
 os.system("cp reachlink_config.exe /root/reachlink/")
 os.system("cp reachlink_hub_config.exe /root/reachlink/")
