c4116ecf3ae9423e25b926c542cfbaa3669689f6
kirishree <115062611+kirishree@users.noreply.github.com>
2025-02-22 12:17:49 +0530

    Update run1.py

--- run1.py
+++ run1.py
@@ -4,5 +4,5 @@ from pymongo.server_api import ServerApi
 import json
 os.system("cp robustel_configure.py /root/reachlink/")
 os.system("cp views_new.py /root/reachlink/reach/views.py")
-os.system("cp reachlinkst.py /root/reachlink/")
-os.system("systemctl restart reachlink_test")
+#os.system("cp reachlinkst.py /root/reachlink/")
+#os.system("systemctl restart reachlink_test")
