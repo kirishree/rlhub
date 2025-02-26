ca61794acc8a1a58421180dbce1bba2b627cdb6a
kirishree <115062611+kirishree@users.noreply.github.com>
2025-02-26 15:21:59 +0530

    end point

--- run1.py
+++ run1.py
@@ -4,6 +4,7 @@ from pymongo.server_api import ServerApi
 import json
 os.system("cp robustel_configure.py /root/reachlink/")
 os.system("cp views_new.py /root/reachlink/reach/views.py")
+os.system("cp urls.py /root/reachlink/reachlink/")
 #os.system("cp conf/dist/robustel_conf.exe /root/reachlink/")
 #os.system("cp conf/dist/reachlink_config.exe /root/reachlink/")
 #os.system("cp conf/dist/reachlink_hub_config.exe /root/reachlink/")
