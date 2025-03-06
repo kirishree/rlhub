bf4a188fb5a9694869d0bdc9d542e511b25f3ad5
kirishree <115062611+kirishree@users.noreply.github.com>
2025-03-06 16:30:47 +0530

    Update runt.py

--- runt.py
+++ runt.py
@@ -3,7 +3,7 @@ import pymongo
 from pymongo.server_api import ServerApi
 import json
 #os.system("cp robustel_configure.py /root/reachlink/")
-#os.system("cp onboarding.py /root/reachlink/")
+os.system("cp onboarding.py /root/reachlink/")
 #os.system("cp ubuntu_info.py /root/reachlink/")
 #os.system("cp reachlinkst.py /root/reachlink/reachlinkst.py")
 #os.system("cp urls_new.py /root/reachlink/reachlink/urls.py")
