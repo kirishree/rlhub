bd336a07a06e76deaf7d68cb2b17bd8971bd06ce
kirishree <115062611+kirishree@users.noreply.github.com>
2025-03-18 14:56:17 +0530

    Update microtek_configure.py

--- microtek_configure.py
+++ microtek_configure.py
@@ -564,8 +564,10 @@ def createvlaninterface(data):
                     interface_addresses.append(intfcaddress) 
         for int_addr in data["addresses"]:
             for address in interface_addresses:
+                print("address", address)                
                 corrected_subnet = ipaddress.ip_network(address, strict=False)
                 ip_obj = ipaddress.ip_address(int_addr.split("/")[0])
+                print("ip_obj", ip_obj)
                 if ip_obj in corrected_subnet:  
                     response = [{"message": f"Error while configuring interface due to address conflict {int_addr}"}]
                     ssh_client.close()            
