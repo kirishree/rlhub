39fb4cf310f03e3716cb5fd3afa5c937667ee424
kirishree <115062611+kirishree@users.noreply.github.com>
2025-03-28 18:37:44 +0530

    Update views_new.py

--- views_new.py
+++ views_new.py
@@ -2465,8 +2465,8 @@ def get_percentile(itemidsent, itemidreceived, itemidping, fromdate):
         "jsonrpc": "2.0",
         "method": "history.get",
         "params": {
-            "output": "extend",     
-            "history": 0,       
+            "output": "extend",    
+                   
             "itemids": [itemidsent, itemidreceived, itemidping],            
             "time_from": int(fromdate),
             "time_till": int(fromdate) + 3600
