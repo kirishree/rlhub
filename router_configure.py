db309e5bdd74af5b8db2b51d1f189a8ca0afe2f6
kirishree <115062611+kirishree@users.noreply.github.com>
2025-05-27 16:04:16 +0530

    Update router_configure.py

--- router_configure.py
+++ router_configure.py
@@ -1028,8 +1028,10 @@ def deletevlaninterface(data):
                 if "interface" in intfc:                    
                     intfc_name = intfc.strip().split("interface")[1]  
                 if "vlan" in intfc and "add" not in intfc:   
-                    print(intfc.split("vlan ")[1])              
+                    print(intfc.split("vlan ")[1])  
+                    print("vlanid", vlanid)            
                     if vlanid == intfc.split("vlan ")[1]:
+                        print(intfc.split("vlan ")[1])  
                         vlanlinkinfo.append({"intfc": intfc_name,
                                          "vlancommand": f'no switchport access vlan {vlanid}'})                                   
         print(vlanlinkinfo)
