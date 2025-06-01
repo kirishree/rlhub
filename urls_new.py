d5f1c83000a59b7221d914cc918430e68d206d1e
cloudetel <142509813+cloudetel@users.noreply.github.com>
2025-06-01 15:13:19 +0300

    Update urls_new.py

--- urls_new.py
+++ urls_new.py
@@ -103,4 +103,3 @@ urlpatterns = [
     path('beapi/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('beapi/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-]
