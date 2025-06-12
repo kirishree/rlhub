12afba6163ce26dbe60f82e9737b1269b5ed178c
kirishree <115062611+kirishree@users.noreply.github.com>
2025-06-12 15:24:39 +0530

    Hub routing testing updated

--- test_views.py
+++ test_views.py
@@ -176,6 +176,14 @@ def test_homepage_info_view_with_token(client, capfd):
     assert "hub_summary" in json_data
     assert "branch_summary" in json_data
 
+def is_excluded(network):
+    return (
+        network.is_loopback or
+        network.is_link_local or
+        network.is_multicast or
+        network.network_address.is_reserved or
+        network.network_address.is_unspecified
+    )
 @override_settings(SECURE_SSL_REDIRECT=False)
 @pytest.mark.django_db
 def test_addstaticroute_hub(client, capfd):
@@ -190,6 +198,7 @@ def test_addstaticroute_hub(client, capfd):
     print("Login response JSON:", login_response.json())
     # Capture output after print
     out, err = capfd.readouterr() 
+    logger.info(f"Login response: {login_response.json()}")
     token = login_response.json().get("access")  # Adjust this if your token key is different
     assert token is not None
 
@@ -197,68 +206,6 @@ def test_addstaticroute_hub(client, capfd):
     headers = {
         "HTTP_AUTHORIZATION": f"Bearer {token}"
     }
-    private_ranges = [
-        (ipaddress.IPv4Network("10.0.0.0/8"), random.randint(8,32)),
-        (ipaddress.IPv4Network("172.16.0.0/12"), random.randint(16,32)),
-        (ipaddress.IPv4Network("192.168.0.0/16"), random.randint(24,32)),
-    ]
-    addroute = []
-    for i in range(0,20):
-        network_base, base_prefix = random.choice(private_ranges)
-        subnets = list(network_base.subnets(new_prefix=base_prefix))
-        subnet_index = random.randint(0, len(subnets)-1)
-        addroute.append({"destination": str(subnets[subnet_index]),
-                         "gateway": "10.8.0.19"})
-    addroute_data = {"hub_wan_ip": "185.69.209.251",
-                     "uuid": "reachlinkserver.net",
-                     "routes_info": addroute}
-    #branch_info_url = reverse("branch_info") + "?organization_id=ea318b0108d6495babfbd020ffc4e132"
-    addstaticroute_hub_url = reverse("addstaticroute_hub")
-    response = client.post(addstaticroute_hub_url, addroute_data, content_type="application/json", **headers)
-
-    assert response.status_code == 200
-    json_data = response.json()
-    print("Homepage Info response JSON:", response.json())
-    # Capture output after print
-    out, err = capfd.readouterr() 
-    # Optional: Assert fields in response
-    assert "Error" not in json_data[0]["message"]
-
-def is_excluded(network):
-    return (
-        network.is_loopback or
-        network.is_link_local or
-        network.is_multicast or
-        network.network_address.is_reserved or
-        network.network_address.is_unspecified
-    )
-
-@override_settings(SECURE_SSL_REDIRECT=False)
-@pytest.mark.django_db
-def test_addstaticroute_hub_allip(client, capfd):
-    # Step 1: Login to get access token
-    login_url = reverse("login_or_register")  # or use hardcoded '/api/auth/'
-    login_data = {
-        "username": "xogaw4457@edectus.com",
-        "password": "xogaw4457@edectus.com"
-    }
-    login_response = client.post(login_url, login_data, content_type="application/json")
-    assert login_response.status_code == 200
-    print("Login response JSON:", login_response.json())
-    # Capture output after print
-    out, err = capfd.readouterr() 
-    token = login_response.json().get("access")  # Adjust this if your token key is different
-    assert token is not None
-
-    # Step 2: Call branch_info with Authorization header
-    headers = {
-        "HTTP_AUTHORIZATION": f"Bearer {token}"
-    }
-    private_ranges = [
-        (ipaddress.IPv4Network("10.0.0.0/8"), random.randint(8,32)),
-        (ipaddress.IPv4Network("172.16.0.0/12"), random.randint(16,32)),
-        (ipaddress.IPv4Network("192.168.0.0/16"), random.randint(24,32)),
-    ]
     addroute = []
     for _ in range(20):
         while True:
@@ -276,15 +223,15 @@ def test_addstaticroute_hub_allip(client, capfd):
     addroute_data = {"hub_wan_ip": "185.69.209.251",
                      "uuid": "reachlinkserver.net",
                      "routes_info": addroute}
-    #branch_info_url = reverse("branch_info") + "?organization_id=ea318b0108d6495babfbd020ffc4e132"
+    logger.info(f"Randomly Generated routes: {addroute}")
     addstaticroute_hub_url = reverse("addstaticroute_hub")
     response = client.post(addstaticroute_hub_url, addroute_data, content_type="application/json", **headers)
-
     assert response.status_code == 200
     json_data = response.json()
-    print("Homepage Info response JSON:", response.json())
+    print("Add Route HUB response JSON:", response.json())
     # Capture output after print
-    out, err = capfd.readouterr() 
+    out1, err = capfd.readouterr() 
+    logger.info(f"Add Route HUB response: {response.json()}")
     # Optional: Assert fields in response
     assert "Error" not in json_data[0]["message"]
     time.sleep(10)
@@ -297,6 +244,9 @@ def test_addstaticroute_hub_allip(client, capfd):
     assert response.status_code == 200
     routing_table = response.json()
     print("routing_table", routing_table)
+     # Capture output after print
+    out2, err = capfd.readouterr() 
+    logger.info(f"Routing table after adding Routes: {routing_table}")
     routenotadded = []
     for addinfo in addroute:
         routeadded = False
@@ -307,6 +257,10 @@ def test_addstaticroute_hub_allip(client, capfd):
         if not routeadded:
             routenotadded.append(addinfo)
     print("Not added route", routenotadded)
+     # Capture output after print
+    out4, err = capfd.readouterr() 
+    if len(routenotadded) > 0:
+        logger.info(f"Not added routes: {routenotadded}")
     assert len(routenotadded) == 0
     
 @override_settings(SECURE_SSL_REDIRECT=False)
@@ -323,6 +277,7 @@ def test_delstaticroute_hub(client, capfd):
     print("Login response JSON:", login_response.json())
     # Capture output after print
     out, err = capfd.readouterr() 
+    logger.info(f"Login response: {login_response.json()}")
     token = login_response.json().get("access")  # Adjust this if your token key is different
     assert token is not None
 
@@ -338,6 +293,10 @@ def test_delstaticroute_hub(client, capfd):
     assert response.status_code == 200
     routing_table = response.json()
     deleteroute = []
+    print("routing table before deletion", routing_table)
+    # Capture output after print
+    out1, err = capfd.readouterr()
+    logger.info(f"Routing table before deletion: {routing_table}")
     for routeinfo in routing_table:
         if routeinfo["protocol"] == "static":
             if "0.0.0.0" not in routeinfo["destination"]:
@@ -353,7 +312,8 @@ def test_delstaticroute_hub(client, capfd):
     json_data = response.json()
     print("Delete Route Info response JSON:", response.json())
     # Capture output after print
-    out, err = capfd.readouterr() 
+    out2, err = capfd.readouterr() 
+    logger.info(f"Delete route response: {json_data}")
     # Optional: Assert fields in response
     assert "Error" not in json_data[0]["message"]
     time.sleep(10)
@@ -365,7 +325,10 @@ def test_delstaticroute_hub(client, capfd):
     response = client.post(getroute_hub_url, getroute_data, content_type="application/json", **headers)
     assert response.status_code == 200
     routing_table = response.json()
-    print("routing_table", routing_table)
+    print("routing_table after deletion", routing_table)
+    # Capture output after print
+    out2, err = capfd.readouterr() 
+    logger.info(f"Routing table after deletion:{routing_table}")
     routenotdeleted = []
     for delinfo in deleteroute:        
         for routeinfo in  routing_table:
@@ -373,6 +336,8 @@ def test_delstaticroute_hub(client, capfd):
                 routenotdeleted.append(delinfo)
                 break               
     print("Not deleted route", routenotdeleted)
+    if len(routenotdeleted) > 0:
+        logger.info(f"Not deleted route: {routenotdeleted}")
     assert len(routenotdeleted) == 0
 
 @override_settings(SECURE_SSL_REDIRECT=False)
