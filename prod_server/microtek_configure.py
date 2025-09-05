b8e884a1cc7025a5f308ca1bcd2f9d0d58a68cd7
kirishree <115062611+kirishree@users.noreply.github.com>
2025-09-05 10:34:39 +0530

    Update microtek_configure.py

--- prod_server/microtek_configure.py
+++ prod_server/microtek_configure.py
@@ -648,15 +648,10 @@ def interfaceconfig(data):
             if "address=" in addr:
                     intfcname = addr.split("interface=")[1].split(" ")[0] 
                     #print(intfcname)
-                    if intfcname == data["intfc_name"]: 
-                            print(addr)                                              
-                            removeitemno = addr.split(" ")[0] 
-                            print("#######",removeitemno) 
-                            if len(removeitemno) > 1:
-                                print(addresses_info[i-1])
-                                print(addresses_info[i-1].strip())
-                                removeitemno = addresses_info[i-1].strip().split(" ")[0]  
-                            print("hiii",removeitemno)                                               
+                    if intfcname == data["intfc_name"]:                                                                          
+                            removeitemno = addr.split(" ")[0]                             
+                            if len(removeitemno) > 1:                                
+                                removeitemno = addresses_info[i-1].strip().split(" ")[0]                                                                          
                             stdin, stdout, stderr = ssh_client.exec_command(f'/ip address remove {removeitemno}')
         
         stdin, stdout, stderr = ssh_client.exec_command(f'/ip address print detail')
