1001f770a591243d7462ed5e119c65738eea7ef5
kirishree <115062611+kirishree@users.noreply.github.com>
2025-09-17 17:09:01 +0530

    Update microtek_configure.py

--- prod_server/microtek_configure.py
+++ prod_server/microtek_configure.py
@@ -760,6 +760,8 @@ def interfaceconfig(data):
             #
             subnet_id_lan = ip_addresses["Subnet_ID"] + "/" + lan_addr.split("/")[1]
             stdin, stdout, stderr = ssh_client.exec_command(f'/ip dhcp-server network remove 0')
+            print("DHCP-Server Remove STDOUT:", stdout.read().decode())
+            print("DHCP-Server Remove STDERR:", stderr.read().decode())
             stdin, stdout, stderr = ssh_client.exec_command(f'/ip dhcp-server network add address="{subnet_id_lan}" gateway="{ip_addr}" dns-server="{ip_addr}"')
             print("DHCP-Server STDOUT:", stdout.read().decode())
             print("DHCP-Server STDERR:", stderr.read().decode())
