0fb086450c55f8ec2f19ee77f48f2719b73aa350
kirishree <115062611+kirishree@users.noreply.github.com>
2025-09-17 18:40:33 +0530

    Update microtek_configure.py

--- prod_server/microtek_configure.py
+++ prod_server/microtek_configure.py
@@ -2424,9 +2424,9 @@ def addfirewallrule(data):
         # Execute the ping command               
         for rule in data["rules"]:   
             if rule["protocol"] == "any" or rule["protocol"] == "all":    
-                stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter add action={rule["action"]} src-address={rule["src_address"]} dst-address={rule["dst_address"]} src-port={rule["src_port"]} dst-port={rule["dst_port"]} place-before=0 comment={rule["description"]}')     
+                stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter add action={rule["action"]} src-address={rule["src_address"]} dst-address={rule["dst_address"]} src-port={rule["src_port"]} dst-port={rule["dst_port"]} comment={rule["description"]}')     
             else:
-                stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter add action={rule["action"]} src-address={rule["src_address"]} dst-address={rule["dst_address"]} src-port={rule["src_port"]} dst-port={rule["dst_port"]} protocol={rule["protocol"]} place-before=0 comment={rule["description"]}')
+                stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter add action={rule["action"]} src-address={rule["src_address"]} dst-address={rule["dst_address"]} src-port={rule["src_port"]} dst-port={rule["dst_port"]} protocol={rule["protocol"]} comment={rule["description"]}')
             # Read the actual output and errors
             #output = stdout.read().decode()
             #if output:               
