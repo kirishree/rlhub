c13f36dfdba25368efa2d96568b85c98e498427b
kirishree <115062611+kirishree@users.noreply.github.com>
2025-09-23 17:23:22 +0530

    Update microtek_configure.py

--- prod_server/microtek_configure.py
+++ prod_server/microtek_configure.py
@@ -2862,8 +2862,8 @@ def editfilterrule(data):
             else:
                 disabled = "no"
             stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter remove {data["rule_no"]}') 
-            time.sleep(2)
-            cmd_parts = [f'/ip firewall filter add place-before={data["rule_no"]} chain={data["chain"]} action={data["action"]} comment="{desc}" disabled={disabled}']
+            time.sleep(20)
+            cmd_parts = [f'/ip firewall filter add chain={data["chain"]} action={data["action"]} comment="{desc}" disabled={disabled}']
 
             if src_addr:
                 cmd_parts.append(f'src-address={src_addr}')
@@ -2875,16 +2875,18 @@ def editfilterrule(data):
                     cmd_parts.append(f'src-port={data["src_port"]}')
                 if data.get("dst_port"):
                     cmd_parts.append(f'dst-port={data["dst_port"]}')
-            
+            cmd_parts.append(f'place-before={data["rule_no"]}')
             cmd = " ".join(cmd_parts)
            
-            stdin, stdout, stderr = ssh_client.exec_command(cmd)       
+            stdin, stdout, stderr = ssh_client.exec_command(cmd)    
+            print("stderr", stderr.read().decode())
+            print("add stdout", stdout.read().decode())               
             #stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter move [find comment="{desc}"] {data["rule_no"]}')
             # Read the actual output and errors
-            print("stderr", stderr.read().decode())
-            output = stdout.read().decode()
-            if output:  
-                print("stdout", output)                    
+            #print("stderr", stderr.read().decode())
+            #output = stdout.read().decode()
+            #if output:  
+            #    print("stdout", output)                    
             response = [{"message": f"Rule edited."}]
         logger.info(
             f"{response}",
