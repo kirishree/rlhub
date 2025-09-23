8db40797b8e9db61ecd0fcfce77427402ee64d9d
kirishree <115062611+kirishree@users.noreply.github.com>
2025-09-23 16:30:20 +0530

    Update microtek_configure.py

--- prod_server/microtek_configure.py
+++ prod_server/microtek_configure.py
@@ -2862,7 +2862,7 @@ def editfilterrule(data):
             else:
                 disabled = "no"
             stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter remove {data["rule_no"]}') 
-            cmd_parts = [f'/ip firewall filter add place-before={data["rule_no"]} chain={data["chain"]} action={data["action"]} comment="{desc}" disabled={disabled}']
+            cmd_parts = [f'/ip firewall filter add chain={data["chain"]} action={data["action"]} comment="{desc}" disabled={disabled}']
 
             if src_addr:
                 cmd_parts.append(f'src-address={src_addr}')
@@ -2878,10 +2878,11 @@ def editfilterrule(data):
             cmd = " ".join(cmd_parts)
            
             stdin, stdout, stderr = ssh_client.exec_command(cmd)       
-             
+            stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter move [find comment="{desc}"] {data["rule_no"]}')
             # Read the actual output and errors
-            #output = stdout.read().decode()
-            #if output:                      
+            output = stdout.read().decode()
+            if output:  
+                print("stdout", output)                    
             response = [{"message": f"Rule edited."}]
         logger.info(
             f"{response}",
