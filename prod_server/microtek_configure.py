a7b10c0db45c4bad381dcaae275bbd349aaed201
kirishree <115062611+kirishree@users.noreply.github.com>
2025-09-23 17:27:14 +0530

    Update microtek_configure.py

--- prod_server/microtek_configure.py
+++ prod_server/microtek_configure.py
@@ -2862,7 +2862,6 @@ def editfilterrule(data):
             else:
                 disabled = "no"
             stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter remove {data["rule_no"]}') 
-            time.sleep(20)
             cmd_parts = [f'/ip firewall filter add chain={data["chain"]} action={data["action"]} comment="{desc}" disabled={disabled}']
 
             if src_addr:
@@ -2875,15 +2874,16 @@ def editfilterrule(data):
                     cmd_parts.append(f'src-port={data["src_port"]}')
                 if data.get("dst_port"):
                     cmd_parts.append(f'dst-port={data["dst_port"]}')
-            cmd_parts.append(f'place-before={data["rule_no"]}')
-            cmd = " ".join(cmd_parts)
-           
+            #cmd_parts.append(f'place-before={data["rule_no"]}')
+            cmd = " ".join(cmd_parts)           
             stdin, stdout, stderr = ssh_client.exec_command(cmd)    
             print("stderr", stderr.read().decode())
-            print("add stdout", stdout.read().decode())               
-            #stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter move [find comment="{desc}"] {data["rule_no"]}')
+            print("add stdout", stdout.read().decode())   
+            time.sleep(20)            
+            stdin, stdout, stderr = ssh_client.exec_command(f'/ip firewall filter move [find comment="{desc}"] {data["rule_no"]}')
             # Read the actual output and errors
-            #print("stderr", stderr.read().decode())
+            print("stderr--move", stderr.read().decode())
+            print("stdout--move", stdout.read().decode())
             #output = stdout.read().decode()
             #if output:  
             #    print("stdout", output)                    
